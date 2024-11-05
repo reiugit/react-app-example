4f03a2aa11b9c99a9f2f3e68f05c63ea68fe83ea
reiugit <67372053+reiugit@users.noreply.github.com>
2024-11-05 11:18:01 +0100

    Added Click event to list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,22 @@
 function ListGroup() {
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  let items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  //items = [];
+
+  const message = items.length === 0 && <p>No items</p>;
 
   return (
     <>
-      <h1></h1>
+      <h3>ListGroup</h3>
+
+      {message}
+
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => console.log({ item })}
+          >
             {item}
           </li>
         ))}
